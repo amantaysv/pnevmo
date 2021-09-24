26b91c9250a176499502b1e82d8b87ee27dce9ff
Amantay <amantay.sv@gmail.com>
2021-09-24 12:11:46 +0600

    fix: removed address

--- src/js/script.js
+++ src/js/script.js
@@ -40,11 +40,14 @@ var map
 
 DG.then(function () {
   map = DG.map('map', {
-    center: [42.894974, 74.627809],
-    zoom: 13,
+    center: [42.883912, 74.665889],
+    zoom: 14,
   })
-  const marker = DG.marker([42.895058, 74.627812]).addTo(map)
-  marker.bindLabel('<p>г.Бишкек, ул.Осмонкулова Бчк Древесная 70/3</p><p>0555-77-65-03, 0550-47-20-71</p><p>Eжедневно с 9:00 до 21:00</p>', { static: true, className: 'map-hint', offset: DG.point(-150, -170) })
+  const marker = DG.marker([42.883992, 74.66497]).addTo(map)
+  marker.bindLabel(
+    '<p>г.Бишкек,  проспект Ленина 314</p><p>0550-82-28-88</p><p>Eжедневно с 9:00 до 21:00</p>',
+    { static: true, className: 'map-hint', offset: DG.point(-150, -170) }
+  )
 })
 
 const modalBtn = document.querySelector('.modal__btn')
